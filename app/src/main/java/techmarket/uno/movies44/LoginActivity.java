6c1158f8fdd62b58a21414b7ac0414d7acbbe8d3
chigov2 <chigov@gmail.com>
2022-02-07 13:58:54 +0200

    Initial commit

---
+++ app/src/main/java/techmarket/uno/movies44/LoginActivity.java
@@ -0,0 +1,17 @@
+package techmarket.uno.movies44;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+}
---
+++ app/src/main/java/techmarket/uno/movies44/RegisterActivity.java
@@ -0,0 +1,30 @@
+package techmarket.uno.movies44;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText editTextEmailAddress;
+    private EditText editTextPassword;
+    private Button buttonRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+    }
+
+    public void onClickAlreadyRegistered(View view) {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+}
